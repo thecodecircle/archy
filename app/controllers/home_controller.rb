2b0edacd1da7f752c2c8d2773f6db77128942139
Jordan Shefner <yarden11111@gmail.com>
2020-10-01 15:58:59 +0300

    Add search and index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :search]
   def index
   end
+
+  def search
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :documents
   devise_for :users
   root to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'search', to: 'home#search'
 end
