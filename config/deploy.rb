9e6a4e1aa229b87094d70568c689be64467f0c4d
Jordan Shefner <yarden11111@gmail.com>
2020-10-13 17:49:32 +0300

    cap stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :linked_files, %w{config/master.key}
 
 ## Defaults:
 # set :scm,           :git
