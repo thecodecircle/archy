6fcb6c98542ec1e4b48ca5a96ca7d6e7686c9bab
Jordan Shefner <yarden11111@gmail.com>
2020-10-13 14:52:52 +0300

    Add hostname to dev.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  config.hosts << "archy.codecircle.co.il"
+  
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
