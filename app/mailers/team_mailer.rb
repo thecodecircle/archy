9b77da428ff421dc0d6598e2c8fd2c7dcb7590dd
Jordan Shefner <yarden11111@gmail.com>
2020-10-21 12:40:20 +0300

    add attachments

--- app/mailers/team_mailer.rb
+++ app/mailers/team_mailer.rb
@@ -4,7 +4,12 @@ class TeamMailer < ApplicationMailer
   def meeting_email(meeting, user)
     @meeting = meeting
     @user = user
-    puts "********************** meeting email ******************"
+    @meeting.attachments.each do |file|
+      attachments[file.blob.filename.to_s] = {
+        mime_type: file.blob.content_type,
+        content: file.blob.download
+      }
+    end
     mail(to: @meeting.team.users.pluck(:email), subject: "סיכום פגישה של #{@meeting.team.name} - ארכיון השומר הצעיר")
   end
 end
